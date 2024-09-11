ff45db4bb9918ec8cf45139278fa6e9e77bb6b3e
fgomes <fcgomes.92@gmail.com>
2024-09-11 00:36:28 -0300

    (feat) update project node dependencies and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		port: process.env.PORT || 5173
+	},
+	preview: {
+		port: process.env.PORT || 5173
 	}
 });
