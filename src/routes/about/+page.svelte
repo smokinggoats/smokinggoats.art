33c6ebb43079df304bca380d4098fe86c6d30597
smoking.goats <fgomes@localhost.localdomain>
2025-07-26 12:22:39 -0300

    (feat) update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 </script>
 
 <div class="page">
-	<h1 class="page__title">About</h1>
+	<div class="page__links">
+		<a class="page__links__link" href="https://linktr.ee/smoking.goats" target="_blank">
+			<Icon class="page__links__link__icon" icon="solar:suitcase-tag-line-duotone" />
+			<span class="page__links__link__text">Smoking Goats - LinkTr</span>
+		</a>
+	</div>
 </div>
 
 <style lang="scss">
@@ -11,11 +17,45 @@
 		box-sizing: border-box;
 		overflow: auto;
 		height: 100%;
-		&__title {
-			font-size: 2rem;
-			margin: 1rem auto;
-			text-align: center;
-			text-transform: uppercase;
+		&__links {
+			margin: 2rem 0;
+			width: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		&__links {
+			&__link {
+				display: flex;
+				align-items: center;
+				justify-content: space-around;
+				cursor: pointer;
+				margin: 0;
+				border: none;
+				padding: 1rem 2rem;
+				border-radius: 10rem;
+				box-shadow:
+					rgb(128, 0, 128) 0px 0px 5px 0px,
+					rgb(255, 192, 203) 0px 0px 5px 0px;
+				background-color: #fff;
+				font-size: 2rem;
+				text-align: center;
+				text-decoration: none;
+				transition: all 0.3s ease;
+				color: #fff;
+				background-color: #555;
+				&__text {
+					margin: 0.5rem;
+				}
+				&:hover {
+					background-color: #fff;
+					color: #000;
+					transform: scale(1.1);
+					box-shadow:
+						rgb(128, 0, 128) 0px 5px,
+						rgb(255, 192, 203) 0px 10px;
+				}
+			}
 		}
 	}
 </style>
